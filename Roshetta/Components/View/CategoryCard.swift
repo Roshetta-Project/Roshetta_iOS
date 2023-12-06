d43b937600c9a38eb82b64386a0d7039e55666a9
Samiiahmed <eldon.smsm135@gmail.com>
2023-12-06 14:28:00 +0200

    add category card - homebanner

---
+++ Roshetta/Components/View/CategoryCard.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryCard.swift
+//  Roshetta
+//
+//  Created by Sami Ahmed on 06/12/2023.
+//
+
+import SwiftUI
+
+struct CategoryCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryCard()
+}
---
+++ Roshetta/Components/View/HomeBanner.swift
@@ -0,0 +1,18 @@
+//
+//  HomeBanner.swift
+//  Roshetta
+//
+//  Created by Sami Ahmed on 06/12/2023.
+//
+
+import SwiftUI
+
+struct HomeBanner: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeBanner()
+}
