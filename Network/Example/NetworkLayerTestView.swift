9209598c8c1d3986fdeaf8db3c01d43707ee341b
Abdalazem Salehm <abdalazem.saleh2@icloud.com>
2024-01-27 01:42:44 +0200

    Add ProductAPIRequest,  ProductAPIClient, and moc model
    hierarchy
    |_📂ProductAPIRequst → that handel needed request for products
    |
    |_📁ProductsAPIClient → contain all API calls  that needed for products.
    |
    |_📁ProductNeworkResponse → The data that come from API without any maping.
    |
    |_📁ProductsViewModel → there is steps before this but in clean arcticture 🧼

--- Network/Example/NetworkLayerTestView.swift
+++ Network/Example/NetworkLayerTestView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct NetworkLayerTestView: View {
+    
+    // MARK: - PROPERTYS
+    private var vm = NetworkLayerViewModel()
+    
+    // MARK: - VIEW
+    
     var body: some View {
         VStack(alignment: .center, spacing: 32) {
             
@@ -15,66 +21,53 @@ struct NetworkLayerTestView: View {
                 .font(.system(size: 18, weight: .bold))
                 .padding(.bottom, 32)
             
+            createButton(title: "Fetch Data", backgroundColor: Colors.text) {
+                Task {
+                    await vm.fetchProducts()
+                }
+            }
+
+            createButton(title: "Post Data", backgroundColor: Colors.secondary) {
+                Task {
+                    await vm.addProduct()
+                }
+            }
             
-            Button {
-                //TODO: - Get data from api
-            } label: {
-                Text("Fetch Data")
-                    .font(.system(size: 16, weight: .bold))
-                    .frame(
-                        maxWidth: .infinity,
-                        minHeight: 48,
-                        maxHeight: 48
-                    )
-                    .padding(.horizontal, 32)
-                    .foregroundColor(Color.white)
-                    .background {
-                        Colors.text
-                    }
-                    .cornerRadius(16)
-                    .shadow(color: Color.black.opacity(0.2), radius: 8)
+            createButton(title: "Multipart data", backgroundColor: Colors.main) {
+                Task {
+                    print("Upload data")
+                }
             }
-
-            Button {
-                //TODO: - Get data from api
-            } label: {
-                Text("Multipart data")
-                    .font(.system(size: 16, weight: .bold))
-                    .frame(
-                        maxWidth: .infinity,
-                        minHeight: 48,
-                        maxHeight: 48
-                    )
-                    .padding(.horizontal, 32)
-                    .foregroundColor(Color.white)
-                    .background {
-                        Colors.main
-                    }
-                    .cornerRadius(16)
-                    .shadow(color: Color.black.opacity(0.2), radius: 8)
-            }
-
-            Button {
-                //TODO: - Get Post
-            } label: {
-                Text("Post Data")
-                    .font(.system(size: 16, weight: .bold))
-                    .frame(
-                        maxWidth: .infinity,
-                        minHeight: 48,
-                        maxHeight: 48
-                    )
-                    .padding(.horizontal, 32)
-                    .foregroundColor(Color.white)
-                    .background {
-                        Colors.secondary
-                    }
-                    .cornerRadius(16)
-                    .shadow(color: Color.black.opacity(0.2), radius: 8)
-            }
+            
         }
         .padding(.horizontal)
+        
     }
+    
+    // MARK: - FUNCTIONS
+    
+    private func createButton(title: String, backgroundColor: Color, action: @escaping () -> Void) -> some View {
+        Button {
+            //TODO: - Get Post
+            action()
+        } label: {
+            Text(title)
+                .font(.system(size: 16, weight: .bold))
+                .frame(
+                    maxWidth: .infinity,
+                    minHeight: 48,
+                    maxHeight: 48
+                )
+                .padding(.horizontal, 32)
+                .foregroundColor(Color.white)
+                .background {
+                    backgroundColor
+                }
+                .cornerRadius(16)
+                .shadow(color: Color.black.opacity(0.2), radius: 8)
+        }
+    }
+    
 }
 
 struct NetworkLayerTestView_Previews: PreviewProvider {
--- Network/Example/NetworkLayerTestViewModel.swift
+++ Network/Example/NetworkLayerTestViewModel.swift
@@ -10,7 +10,11 @@ import Foundation
 class NetworkLayerViewModel: ObservableObject {
     
     // MARK: - PROPERTYS
+    let productApiClient = ProductApiClient(client: BaseAPIClient())
     
+    @Published var products: [ProductNetworkResponse]?
+    @Published var addedProduct: ProductNetworkResponse?
+    @Published var isError: Bool = false
     
     // MARK: - INITILIZER
     
@@ -18,4 +22,116 @@ class NetworkLayerViewModel: ObservableObject {
         
     // MARK: - FUNCTIONS
     
+     func fetchProducts() async {
+        do {
+            products = try await productApiClient.fetchProducts()
+            guard  let products = products  else { return }
+            print(products)
+        } catch {
+            print("Error is \(error.localizedDescription)")
+            isError = true
+        }
+    }
+    
+     func addProduct() async {
+        do {
+            let parameters = UserDetailsParameters(productParameters: [
+                "title":"test product",
+                "price": 13,
+                "description":"lorem ipsum set",
+                "image":"https://i.pravatar.cc",
+                "category":"electronic"
+            ])
+            addedProduct = try await productApiClient.addProduct(parameters: parameters)
+            guard  let addedProduct = addedProduct  else { return }
+            print(addedProduct)
+        } catch {
+            print("Error is \(error.localizedDescription)")
+            isError = true
+        }
+    }
+    
+    private struct UserDetailsParameters: ProductParametersProtocol {
+        var productParameters: [String : Any]
+    }
+}
+
+// MARK: - Products API Request
+enum ProductsAPIRequest: APIRequestConfiguration {
+    case fetchProducts
+    case addProduct(parameters: ProductParametersProtocol)
+    
+    var method: HTTPMethod {
+        switch self {
+        case .fetchProducts:
+            return .get
+        case .addProduct:
+            return .post
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .fetchProducts:
+            return "/products"
+        case .addProduct:
+            return "/products"
+        }
+    }
+    
+    var parameters: Parameters? {
+        switch self {
+        case .fetchProducts:
+            return nil
+        case .addProduct(let parameters):
+            return parameters.productParameters
+        }
+    }
+
+    var headers: HTTPHeaders? {
+      nil
+    }
+}
+
+/// dependency injection
+protocol ProductParametersProtocol {
+    var productParameters: [String: Any] { get }
+}
+
+// MARK: - Product API Client
+
+protocol ProductAPIClientProtocol {
+  func fetchProducts() async throws -> [ProductNetworkResponse]?
+  func addProduct(parameters: ProductParametersProtocol) async throws -> ProductNetworkResponse?
+}
+
+class ProductApiClient: ProductAPIClientProtocol {
+    let client: BaseAPIClientProtocol
+    init(client: BaseAPIClientProtocol) {
+        self.client = client
+    }
+    
+    func fetchProducts() async throws -> [ProductNetworkResponse]? {
+        let request = ProductsAPIRequest.fetchProducts
+        var products: [ProductNetworkResponse]?
+        products = try await client.perform(request)
+        return products
+    }
+    
+    func addProduct(parameters: ProductParametersProtocol) async throws -> ProductNetworkResponse? {
+        let request = ProductsAPIRequest.addProduct(parameters: parameters)
+        var product: ProductNetworkResponse?
+        product = try await client.perform(request)
+        return product
+    }
+}
+
+
+// MARK: - Model
+
+struct ProductNetworkResponse: Codable {
+    var id: Int?
+    var title: String?
+    var category: String?
+    var description: String?
 }
