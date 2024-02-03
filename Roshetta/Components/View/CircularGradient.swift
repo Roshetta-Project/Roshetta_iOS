1b6d5fe3b7fad7d4dba4a1308ac6ccb3d6dc5935
Samiiahmed <eldon.smsm135@gmail.com>
2024-02-03 14:13:31 +0200

    edit AuthView

---
+++ Roshetta/Components/View/CircularGradient.swift
@@ -0,0 +1,18 @@
+//
+//  CircularGradient.swift
+//  Roshetta
+//
+//  Created by Sami Ahmed on 03/02/2024.
+//
+
+import SwiftUI
+
+struct CircularGradient: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularGradient()
+}
