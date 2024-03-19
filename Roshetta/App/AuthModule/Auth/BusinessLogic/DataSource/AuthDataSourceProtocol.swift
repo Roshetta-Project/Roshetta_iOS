e3c6a5bcb228adeb9dcf22dee6a4c2cea6c118b0
Abdalazem Salehm <abdalazem.saleh2@icloud.com>
2024-03-19 22:43:05 +0200

    Statge AuthDataSourceProtocol

--- Roshetta/App/AuthModule/Auth/BusinessLogic/DataSource/AuthDataSourceProtocol.swift
+++ Roshetta/App/AuthModule/Auth/BusinessLogic/DataSource/AuthDataSourceProtocol.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+/// Solve this issus for sami ahmed osman mehid
+
+protocol AuthDataSourceProtocol {
+    var user: UserProtocol? { get set }
+    var isLoading: Bool { get set }
+}
