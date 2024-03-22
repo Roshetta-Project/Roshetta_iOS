e8ab347c9a697ad4874730ef279e00b527b2f468
Samiiahmed <eldon.smsm135@gmail.com>
2024-03-22 04:41:41 +0200

    edit onboarding screens

--- Roshetta/App/Onboarding/Model/onboardingModel.swift
+++ Roshetta/App/Onboarding/Model/onboardingModel.swift
@@ -19,15 +19,15 @@ extension OnbordingModel {
     static let onboardingData: [OnbordingModel] = [
         .init(id: 0,
               image: Images.onboarding1,
-              title: "Find Professional Doctor.",
-              descrption: "You can search for doctors based on their specialization, ensuring that you find the right healthcare provider for your specific needs, whether it's a general practitioner or specialist."),
+              title: "Who we are .",
+              descrption: "You can search for doctors based on their specialization, ensuring that you find the right healthcare provider for your specific needs."),
         .init(id: 1,
               image: Images.onboarding2,
-              title: "Your family is in good hands.",
-              descrption: "Our app places a strong emphasis on family well-being, offering comprehensive healthcare solutions for both you and your cherished family members."),
+              title: "Our vision",
+              descrption: "Is to empower users with personalized health management and seamless connectivity to healthcare providers for improved outcomes."),
         .init(id: 2,
               image: Images.onboarding3,
-              title: "You have our support, always.",
-              descrption: "The app provides a dedicated support channel, such as a help center or a chat support feature, where users can seek assistance for any questions or issues they encounter while using the app.")
+              title: "Why us",
+              descrption: "Choose us for our commitment to user-centric design, innovative features tailored for optimal health management.")
     ]
 }
--- Roshetta/App/Onboarding/View/OnboardingView.swift
+++ Roshetta/App/Onboarding/View/OnboardingView.swift
@@ -16,18 +16,23 @@ struct OnboardingView: View {
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffset: CGFloat = 0
     let hapticFeedback = UINotificationFeedbackGenerator()
-
+    
     @State private var currentStep: Int = 0
     @State private var isAnimating: Bool = false
-        
+    
+    @State private  var isLoading = false
+    
+    
+    
+    
     // MARK: - VIEW
     var body: some View {
         ZStack {
-            VStack(spacing: 32) {
+            VStack(spacing: 16) {
                 tabView()
                 currentPage()
                 footer()
-                .padding(.horizontal, 32)
+                    .padding(.horizontal, 32)
             }
             .padding(.top, 64)
             .padding(.bottom, 32)
@@ -45,11 +50,11 @@ struct OnboardingView: View {
                     
                     VStack(spacing: 16) {
                         Text(tab.title)
-                            .font(.custom(GFFonts.roman, size: 24))
-                            .foregroundColor(Colors.text)
+                            .font(.custom(GFFonts.SeguiSemiBold, size: 24))
+                            .foregroundColor(Color.black)
                         
                         Text(tab.descrption)
-                            .font(.custom(GFFonts.popinsMedium, size: 16))
+                            .font(.custom(GFFonts.Segui, size: 16))
                             .multilineTextAlignment(.center)
                             .foregroundColor(.gray.opacity(0.7))
                             .padding(.horizontal)
@@ -66,6 +71,7 @@ struct OnboardingView: View {
     }
     
     // MARK: - CURRENT PAGE
+    
     private func currentPage() -> some View {
         HStack {
             ForEach(data.indices, id: \.self) { index in
@@ -84,22 +90,12 @@ struct OnboardingView: View {
         }
         .padding(.bottom)
     }
-
+    
     // MARK: - FOOTER
     private func footer() -> some View {
-        HStack {
-            if currentStep < data.count - 1 {
-                Button {
-                    isOnboardingViewActive = false
-                } label: {
-                    Text("Skip")
-                        .font(.custom(GFFonts.popinsBold, size: 14))
-                        .foregroundColor(Colors.text)
-                }
-
-                Spacer()
-                
-                GFNextButton {
+        VStack (spacing:16){
+            if currentStep < data.count  {
+                GFButton(isLoading: $isLoading, text: "Next", backgroundColor: Colors.main, foregroundColot: Color.white) {
                     if currentStep < data.count - 1 {
                         isAnimating = true
                         currentStep += 1
@@ -107,15 +103,19 @@ struct OnboardingView: View {
                         isOnboardingViewActive = false
                     }
                 }
-            } else {
-                GFSliderButton(isAnimating: $isAnimating) {
-                    buttonOffset = buttonWidth - 80
-                    isOnboardingViewActive = false
-                }
+            }
+            
+            Button {
+                isOnboardingViewActive = false
+            } label: {
+                Text("Skip")
+                    .font(.custom(GFFonts.popinsBold, size: 16))
+                    .foregroundColor(Color.gray)
             }
         }
     }
 }
+
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
