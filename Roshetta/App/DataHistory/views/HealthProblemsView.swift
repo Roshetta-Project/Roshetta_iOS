ab5d8ef60f44935dd3cd0211f2b6ddd700f88054
Abdelrahman Esmail <pro.abdelrahmanismail@gmail.com>
2024-04-02 11:33:32 +0200

    Enhanced HealthProblemsView Created HealthProblemToggle Component

--- Roshetta/App/DataHistory/views/HealthProblemsView.swift
+++ Roshetta/App/DataHistory/views/HealthProblemsView.swift
@@ -11,13 +11,14 @@ struct HealthProblemsView: View {
     // MARK: - Properties
     @State private var isLoading = false
     @State private var healthProblems = [
-        "Diabetes","Hypertension","Pregnancy","Insulin Resistance","Pco",
-        "Favism","Autoimmune and Inflammation","Hypertension and Diabetes",
-        "Gout","Gastric Sleeve","High Cholesterol","Lactose Intolerance",
-        "Hepatic Patient","Familial Mediterranean","Kidney Disease",
-        "Hypertension and Autoimmune Diseases","Hyperthyroidism","Hypothyroidism"
+        "Diabetes", "Hypertension", "Pregnancy", "Insulin Resistance", "Pco",
+        "Favism", "Autoimmune and Inflammation", "Hypertension and Diabetes",
+        "Gout", "Gastric Sleeve", "High Cholesterol", "Lactose Intolerance",
+        "Hepatic Patient", "Familial Mediterranean", "Kidney Disease",
+        "Hypertension and Autoimmune Diseases", "Hyperthyroidism", "Hypothyroidism"
     ]
     @State private var selectedHealthProblems: [String: Bool] = [:]
+    let numberOfColumns = 3
     
     // MARK: - Body
     var body: some View {
@@ -29,27 +30,33 @@ struct HealthProblemsView: View {
                     .font(.custom(GFFonts.SeguiSemiBold, size: 24))
                     .padding(.top, 50)
                 
-                
-                    ForEach(healthProblems, id: \.self) { problem in
-                            Toggle(isOn: Binding(
-                                get: { selectedHealthProblems[problem] ?? false },
-                                set: { selectedHealthProblems[problem] = $0 }
-                            ), label: {
-                                Text(problem)
-                            }).toggleStyle(.button)
-                    }
-                
+                healthProblemsGrid
                 
                 Spacer()
-                // MARK: - Next Button
-                GFButton(isLoading: $isLoading, text: "Next", backgroundColor: Colors.main, foregroundColot: Color.white) {}
                 
+                nextButton
             }
             .padding()
         }
     }
+    
+    private var healthProblemsGrid: some View {
+        LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: numberOfColumns), spacing: 10){
+            ForEach(healthProblems, id: \.self) { problem in
+                Toggle(isOn: Binding(
+                    get: { selectedHealthProblems[problem] ?? false },
+                    set: { selectedHealthProblems[problem] = $0 }
+                ), label: {
+                    Text(problem)
+                }).toggleStyle(HealthProblemToggle(isSelected: selectedHealthProblems[problem] ?? false))
+            }
+        }
+    }
+    
+    private var nextButton: some View {
+            GFButton(isLoading: $isLoading, text: "Next", backgroundColor: Colors.main, foregroundColot: Color.white) {}
+        }
 }
-
 
 
 #Preview {
---
+++ Roshetta/Components/View/HealthProblemToggle.swift
@@ -0,0 +1,34 @@
+//
+//  HealthProblemToggle.swift
+//  Roshetta
+//
+//  Created by Abdelrahman Esmail on 02/04/2024.
+//
+
+import SwiftUI
+
+struct HealthProblemToggle: ToggleStyle {
+    let isSelected: Bool
+    let cardWidth = (UIScreen.main.bounds.width / 3) - 35
+    
+    func makeBody(configuration: Configuration) -> some View {
+        Button(action: {
+            configuration.isOn.toggle()
+        }, label: {
+            HStack {
+                configuration.label
+                    .frame(width: cardWidth)
+                    .lineLimit(2)
+                    .font(.custom(GFFonts.Segui, size: 16))
+                    .minimumScaleFactor(0.5)
+                    .foregroundColor(isSelected ? Color.white : Color.gray)
+                    .padding(8)
+                    .background(isSelected ? Colors.main : Color.gray.opacity(0.05))
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(isSelected ? Colors.main : Color.gray, lineWidth: 1))
+            }
+        })
+    }
+}
