c1e6ac6d2372c4b40c9fea563bd18c5a2887ac6d
Abdelrahman Esmail <pro.abdelrahmanismail@gmail.com>
2024-04-14 06:08:09 +0200

    Created TimeScrollView

--- Roshetta/App/Detaild Screen/BookingView.swift
+++ Roshetta/App/Detaild Screen/BookingView.swift
@@ -9,52 +9,54 @@ import SwiftUI
 
 struct BookingView: View {
     // MARK: - Properties
-    @State private var selectedDayIndex = 0
-    @State private var selectedTimeIndex = 0
-    
-    let days = ["Sat","Sun", "Mon", "Tue", "Wed", "Thu", "Fri"]
-    let times = ["10:00 AM", "11:00 AM", "1:00 PM", "2:00 PM", "3:00 PM"]
-    
-    init(){
-        UISegmentedControl.appearance().selectedSegmentTintColor = UIColor.init(red: 0.44, green: 0.76, blue: 0.97, alpha: 1.00)
-        UISegmentedControl.appearance().backgroundColor = .white
-        let selectedAttributes: [NSAttributedString.Key:Any] = [
-            .foregroundColor : UIColor.black,
-        ]
-        UISegmentedControl.appearance().setTitleTextAttributes(selectedAttributes, for: .selected)
-        let normalAttributes: [NSAttributedString.Key:Any] = [
-          .foregroundColor : UIColor.lightGray,
-        ]
-        UISegmentedControl.appearance().setTitleTextAttributes(normalAttributes, for: .normal)
-    }
+//    @State private var selectedDayIndex = 0
+//    @State private var selectedTimeIndex = 0
+//    
+//    let days = ["Sat","Sun", "Mon", "Tue", "Wed", "Thu", "Fri"]
+//    let times = ["10:00 AM", "11:00 AM", "1:00 PM", "2:00 PM", "3:00 PM"]
+//    
+//    init(){
+//        UISegmentedControl.appearance().selectedSegmentTintColor = UIColor.init(red: 0.44, green: 0.76, blue: 0.97, alpha: 1.00)
+//        UISegmentedControl.appearance().backgroundColor = .white
+//        let selectedAttributes: [NSAttributedString.Key:Any] = [
+//            .foregroundColor : UIColor.black,
+//        ]
+//        UISegmentedControl.appearance().setTitleTextAttributes(selectedAttributes, for: .selected)
+//        let normalAttributes: [NSAttributedString.Key:Any] = [
+//            .foregroundColor : UIColor.lightGray,
+//        ]
+//        UISegmentedControl.appearance().setTitleTextAttributes(normalAttributes, for: .normal)
+//    }
     
     // MARK: - Body
     var body: some View {
         VStack(alignment: .leading) {
-                Text("Select Date:")
-                .font(.custom(GFFonts.SeguiSemiBold, size: 18))
-//                Picker(selection: $selectedDayIndex, label: Text("")) {
-//                    ForEach(0..<7) { index in
-//                        Button(action: {}) {
-//                            Text(self.days[index])
-//                        }
-//                    }
-//                }
-//                .pickerStyle(SegmentedPickerStyle())
-                
-                DaysDateScrollView()
-                
-                Text("Select Schedule Day:")
-                Picker(selection: $selectedTimeIndex, label: Text("")) {
-                    ForEach(0..<5) { index in
-                        Button(action: {}) {
-                            Text(self.times[index])
-                        }
-                    }
-                }
-                .pickerStyle(SegmentedPickerStyle())
-            }
-            .padding()
+            Text("Select Date")
+            
+            //                Picker(selection: $selectedDayIndex, label: Text("")) {
+            //                    ForEach(0..<7) { index in
+            //                        Button(action: {}) {
+            //                            Text(self.days[index])
+            //                        }
+            //                    }
+            //                }
+            //                .pickerStyle(SegmentedPickerStyle())
+            
+            DaysDateScrollView()
+            
+            Text("Select Schedule Day")
+            TimeScrollView()
+            //                Picker(selection: $selectedTimeIndex, label: Text("")) {
+            //                    ForEach(0..<5) { index in
+            //                        Button(action: {}) {
+            //                            Text(self.times[index])
+            //                        }
+            //                    }
+            //                }
+            //                .pickerStyle(SegmentedPickerStyle())
+        }
+        .font(.custom(GFFonts.SeguiSemiBold, size: 18))
+        .padding()
         Spacer()
     }
 }
--- Roshetta/Components/View/TimeScrollView.swift
+++ Roshetta/Components/View/TimeScrollView.swift
@@ -1,22 +1,26 @@
 //
-//  DaysDateScrollView.swift
+//  TimeScrollView.swift
 //  Roshetta
 //
-//  Created by Abdelrahman Esmail on 08/04/2024.
+//  Created by Abdelrahman Esmail on 14/04/2024.
 //
 
 import SwiftUI
 
-struct DaysDateScrollView: View {
+struct TimeScrollView: View {
     // MARK: - Properties
-    let days = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"]
+    let times = [
+        "2:30 pm", "3:00 pm", "3:30 pm", "4:00 pm", "4:30 pm", "5:00 pm",
+        "5:30 pm", "6:00 pm", "6:30 pm", "7:00 pm", "7:30 pm", "8:00 pm",
+        "8:30 pm", "9:00 pm", "9:30 pm"
+    ]
     @State private var selectedDayIndex: Int? = nil
     
     // MARK: - Body
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(spacing: 10) {
-                ForEach(days.indices, id: \.self) { index in
+                ForEach(times.indices, id: \.self) { index in
                     Button {
                         if selectedDayIndex == index {
                             // If the same day is clicked again, deselect it
@@ -26,11 +30,11 @@ struct DaysDateScrollView: View {
                             selectedDayIndex = index
                         }
                     } label: {
-                        Text(days[index])
+                        Text(times[index])
                             .padding(.vertical, 8)
-                            .padding(.horizontal, 16)
+                            .padding(.horizontal, 10)
                             .foregroundStyle(.black)
-                            .font(.custom(GFFonts.SeguiSemiBold, size: 16))
+                            .font(.custom(GFFonts.SeguiSemiBold, size: 14))
                             .background(
                                 RoundedRectangle(cornerRadius: 8)
                                     .foregroundColor(selectedDayIndex == index ? Colors.main : Color.white)
@@ -44,5 +48,5 @@ struct DaysDateScrollView: View {
 }
 
 #Preview {
-    DaysDateScrollView()
+    TimeScrollView()
 }
