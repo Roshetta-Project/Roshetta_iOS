9485948a5f752e551111f8d849d8bb2173d01edc
Abdalazem Saleh <abdalazem.saleh2@icloud.com>
2024-06-20 20:44:42 +0300

    commmite

--- Roshetta/App/DoctorList/ViewModel/DoctorViewModel.swift
+++ Roshetta/App/DoctorList/ViewModel/DoctorViewModel.swift
@@ -19,8 +19,15 @@ class DoctorViewModel: ObservableObject {
     @MainActor
     func getDoctors() async {
         guard let url = URL(string: "https://roshetta-wy5u.onrender.com/api/v1/doctors") else { return }
+        
+        var request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue(
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YzU2MTEyOGQwMjM1YjJlNDk5M2I0ZiIsImlhdCI6MTcxMjAwMDAxMiwiZXhwIjoxNzE5Nzc2MDEyfQ.DBtZ_TDlJHnIp1arezAMwtV_-vb-GJ5L1kHMREg3yZ8",
+            forHTTPHeaderField: "Authorization")
+
         do {
-            let (data, response) = try await URLSession.shared.data(from: url)
+            let (data, response) = try await URLSession.shared.data(for: request)
             guard (response as? HTTPURLResponse)?.statusCode == 200 else {
                 throw URLError(.badServerResponse)
             }
