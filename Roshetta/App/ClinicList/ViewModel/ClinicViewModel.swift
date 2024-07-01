af34a86010682866227082ffa10fd86aa3dd291b
Samiiahmed <eldon.smsm135@gmail.com>
2024-07-01 11:49:16 +0300

    sss

--- Roshetta/App/ClinicList/ViewModel/ClinicViewModel.swift
+++ Roshetta/App/ClinicList/ViewModel/ClinicViewModel.swift
@@ -14,26 +14,29 @@ class ClinicViewModel: ObservableObject {
     
     @MainActor
     func getClinic() async {
-        guard let url = URL(string: "https://roshetta-back.vercel.app/api/v1/clinics") else { return }
-        
-        var request = URLRequest(url: url)
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue(
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YzU2MTEyOGQwMjM1YjJlNDk5M2I0ZiIsImlhdCI6MTcxOTc3OTY1NCwiZXhwIjoxNzI3NTU1NjU0fQ.GZcibPXxYgHCakpxWjbRoPIFx2PfSF14mLYgyowqCIg",
-            forHTTPHeaderField: "Authorization")
-        
-        do {
-            let (data, response) = try await URLSession.shared.data(for: request)
-            guard (response as? HTTPURLResponse)?.statusCode == 200 else {
-                throw URLError(.badServerResponse)
-            }
+        if let user: UserModel = UserDefaults.standard.getUser(forKey: "cachedUser") {
             
-            let decoder = JSONDecoder()
-            let clinicsResponse = try decoder.decode(ClinicListModel.self, from: data)
-            clinics = clinicsResponse.data
-            status = .success
-        } catch {
-            status = .error(error.localizedDescription)
+            guard let url = URL(string: "https://roshetta-back.vercel.app/api/v1/clinics") else { return }
+            
+            var request = URLRequest(url: url)
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue(
+                "Bearer \(user.data.token)",
+                forHTTPHeaderField: "Authorization")
+            
+            do {
+                let (data, response) = try await URLSession.shared.data(for: request)
+                guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+                    throw URLError(.badServerResponse)
+                }
+                
+                let decoder = JSONDecoder()
+                let clinicsResponse = try decoder.decode(ClinicListModel.self, from: data)
+                clinics = clinicsResponse.data
+                status = .success
+            } catch {
+                status = .error(error.localizedDescription)
+            }
         }
     }
 }
