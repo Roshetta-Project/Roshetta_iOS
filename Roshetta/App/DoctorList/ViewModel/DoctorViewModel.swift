437c3853f35811efdc07b141fe98793e0245fc54
Abdalazem Saleh <abdalazem.saleh2@icloud.com>
2024-07-01 11:27:17 +0300

    token

--- Roshetta/App/DoctorList/ViewModel/DoctorViewModel.swift
+++ Roshetta/App/DoctorList/ViewModel/DoctorViewModel.swift
@@ -19,34 +19,37 @@ class DoctorViewModel: ObservableObject {
     
     // Fillter Key ->
     func getDoctors() async {
-        guard let url = URL(string: "https://roshetta-back.vercel.app/api/v1/doctors") else {
-            status = .error("Invalid URL")
-            return
-        }
-        
-        var request = URLRequest(url: url)
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue(
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YzU2MTEyOGQwMjM1YjJlNDk5M2I0ZiIsImlhdCI6MTcxOTc3OTY1NCwiZXhwIjoxNzI3NTU1NjU0fQ.GZcibPXxYgHCakpxWjbRoPIFx2PfSF14mLYgyowqCIg",
-            forHTTPHeaderField: "Authorization")
-        
-        do {
-            let (data, response) = try await URLSession.shared.data(for: request)
-            guard (response as? HTTPURLResponse)?.statusCode == 200 else {
-                throw URLError(.badServerResponse)
+        if let user: UserModel = UserDefaults.standard.getUser(forKey: "cachedUser") {
+            
+            guard let url = URL(string: "https://roshetta-back.vercel.app/api/v1/doctors") else {
+                status = .error("Invalid URL")
+                return
             }
             
-            if let jsonString = String(data: data, encoding: .utf8) {
-                print("Response is ---")
-               print(jsonString)
+            var request = URLRequest(url: url)
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue(
+                "Bearer \(user.data.token)",
+                forHTTPHeaderField: "Authorization")
+            
+            do {
+                let (data, response) = try await URLSession.shared.data(for: request)
+                guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+                    throw URLError(.badServerResponse)
+                }
+                
+                if let jsonString = String(data: data, encoding: .utf8) {
+                    print("Response is ---")
+                    print(jsonString)
+                }
+                
+                let decoder = JSONDecoder()
+                let doctorsResponse = try decoder.decode(DoctorListModel.self, from: data)
+                doctors = doctorsResponse.data
+                status = .success
+            } catch {
+                status = .error(error.localizedDescription)
             }
-
-            let decoder = JSONDecoder()
-            let doctorsResponse = try decoder.decode(DoctorListModel.self, from: data)
-            doctors = doctorsResponse.data
-            status = .success
-        } catch {
-            status = .error(error.localizedDescription)
         }
     }
 }
